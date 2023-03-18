f92cdacbd25c3752ee6ddbc40e23d7317762b775
illast <illast@ttu.ee>
2023-03-18 17:26:30 +0200

    Initial commit

---
+++ src/main/java/com/example/fujitsutask/FujitsuTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.fujitsutask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujitsuTaskApplication.class, args);
+	}
+
+}
